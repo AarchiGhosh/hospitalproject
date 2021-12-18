e96e4771411c1ef61f7cac51dc417f65fc79bb89
AarchiGhosh <aarchighosh@gmail.com>
2021-12-19 00:44:16 +0530

    merge conflicts

--- hospital/views.py
+++ hospital/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'hospital/base.html',{'year':"2022"})
+    return render(request, 'hospital/404.html',{'header':"Hospital Admission"})
