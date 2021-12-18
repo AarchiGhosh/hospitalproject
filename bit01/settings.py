be2964f481f2de0df4b2fcc2b3ffc82a2edcf1c2
krud97 <krithik.577@gmail.com>
2021-12-18 23:49:53 +0530

    Added Login and Signup Views

--- bit01/settings.py
+++ bit01/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'hospital.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
